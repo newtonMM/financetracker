975d693c7684a36eaaf027473e9ea059c43b077e
Dennis Macharia <dennismacharia4747@gmail.com>
2019-08-13 10:00:47 +0300

    fix server not running

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const router = require('./routes');
+const router = require('./server/routes');
+
 require('dotenv');
 
 const bodyParser = require('body-parser');
