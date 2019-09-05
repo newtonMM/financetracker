8aab74ca2e68fe597f99d578063af384d5a07c24
Dennis Macharia <dennismacharia4747@gmail.com>
2019-09-05 18:10:33 +0300

    - add eslint and babel - fix login and register controllers

--- server/routes/user.js
+++ server/routes/user.js
@@ -3,10 +3,10 @@ const userControl = require('../controllers/user');
 const router = express.Router();
 
 router.route('/login').post(userControl.login);
-router.use(userControl.authenticate);
-
 router.route('/logout').post(userControl.logout);
 router.route('/register').post(userControl.register);
+router.use(userControl.authenticate);
+
 router
   .route('/:userId')
   .put(userControl.updateUser)
